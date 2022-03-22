1e398128aed9308a447322b84d85c53b3df42129
Dan <spinydaniel@gmail.com>
2022-03-23 01:04:21 +0300

    Improve and correct remove_spaces script and update related data pipeline

--- starter/remove_spaces.py
+++ starter/remove_spaces.py
@@ -5,4 +5,20 @@ CORRECTED_DATA_PATH = '../data/census_cor.csv'
 
 df = pd.read_csv(DATA_PATH)
 df.columns = df.columns.str.replace(' ', '')
+cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
+
+for col in cat_features:
+    df[col] = df[col].str.replace(' ', '')
+
 df.to_csv(CORRECTED_DATA_PATH, index=False)
+
+
